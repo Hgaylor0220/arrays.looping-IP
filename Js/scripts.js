44512ca9dcec2fc7f6cd47feb0e6a15358ad4cce
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-06-23 09:04:38 -0700

    Add files via upload
    first commit

---
+++ Js/scripts.js
@@ -0,0 +1,46 @@
+// business logic
+var pingPong = function(digit) {
+  if ((entry % 3 === 0 ) && (entry % 5 === 0 ) && (entry % 15 === 0) || (entry !== num)) {
+    return true;
+  } else {
+    return false;
+
+var pingPong = (15,30,45,60);
+    var ping = (3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60);
+    var pong = (5,10,15,20,25,30,35,40,45,50,55,60);
+    // var result = (entry);
+    var digit = parseInt($("input#digit").val());
+
+      // if ((ping).show("Ping!")||(pong).show("Pong"(pingPong)||.show("PingPong!!")){
+
+
+    if (fasle);
+      return ("Please enter a positive number between 1 and 60")
+      console.log(result)
+    }
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("form#pingPong").submit(function(event) {
+    event.preventDefault();
+    // var pingPong = (15,30,45,60);
+    // var ping = (3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60);
+    // var pong = (5,10,15,20,25,30,35,40,45,50,55,60);
+    // var result = ("pingPong"(entry));
+    var digit = parseInt($("input#digit").val());
+//     var year = parseInt($("input#year").val());
+    // var result = pingPong (digit);
+
+    $(".digit").text(digit);
+
+    if (!result) {                 // same as writing if (result === false)
+      return ("please enter a positive number between 1-60");
+    }else if
+      return ( ping.show("Ping!")|| pong .show("Pong" pingPong ||.show("PingPong!!");
+    }
+//
+    $("#result").show("#result");
+    console.log()
+  });
+});
---
+++ ReadMe.md
@@ -0,0 +1,38 @@
+# _{JavaScript Project}_
+
+#### _{Juraasic Park Character Questionair}, {June 9th 2019}_
+
+#### By _Hailey J Gaylor_
+
+## Description
+
+_{blah blah blah... blah blah, blah blah blah blah blah blah.. }_
+
+## Setup/Installation Requirements
+
+* _Chrome is recommended_
+* _Read line by line on page_
+* _Close browser_
+* _Open new browser_
+* _Eat a sandwich_
+* _Markdown text_
+
+_{Keep trying and if you do not succeed eat the sandwich, close your browser and open a new one. Don't be tempted by defeat. Try something new.}_
+
+## Known Bugs
+
+_{No known bugs at this time. }_
+
+## Support and contact details
+
+_{None, all were eaten}_
+
+## Technologies Used
+
+_{Using javaScrip, CSS and JQuery.}_
+
+### License
+
+*{No License needed or used, dont worry Im legit ;)}*
+
+Copyright (c) 1992 **_{My mom, Shannon, My dad, Khun. Hailey Gaylor}_**
